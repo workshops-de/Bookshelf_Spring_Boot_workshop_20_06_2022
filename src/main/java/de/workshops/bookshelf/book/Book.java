deefc4fc9e4a1846f8022202faa565bc785ad00d
Birgit Kratz <mail@birgitkratz.de>
2022-06-20 14:38:24 +0200

    implement BookRestController with getAllBooks

---
+++ src/main/java/de/workshops/bookshelf/book/Book.java
@@ -0,0 +1,11 @@
+package de.workshops.bookshelf.book;
+
+import lombok.Data;
+
+@Data
+public class Book {
+    private String title;
+    private String description;
+    private String author;
+    private String isbn;
+}
---
+++ src/main/java/de/workshops/bookshelf/book/BookRestController.java
@@ -0,0 +1,38 @@
+package de.workshops.bookshelf.book;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.PostConstruct;
+import java.io.IOException;
+import java.util.List;
+
+@RestController
+@RequestMapping("book")
+public class BookRestController {
+
+    private final ObjectMapper mapper;
+    private final ResourceLoader resourceLoader;
+
+    private List<Book> books;
+
+    public BookRestController(ObjectMapper mapper, ResourceLoader resourceLoader) {
+        this.mapper = mapper;
+        this.resourceLoader = resourceLoader;
+    }
+
+    @PostConstruct
+    public void init() throws IOException {
+        final var resource = resourceLoader.getResource("classpath:books.json");
+        books = mapper.readValue(resource.getInputStream(), new TypeReference<>() {});
+    }
+
+    @GetMapping
+    List<Book> getAllBooks() {
+        return books;
+    }
+}
