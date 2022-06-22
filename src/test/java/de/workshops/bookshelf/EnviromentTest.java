38af1f5223e073183bb4ecf2beee9ad35f98873c
Birgit Kratz <mail@birgitkratz.de>
2022-06-22 07:44:08 +0200

    introduce configuration for profiles, test a profile

---
+++ src/test/java/de/workshops/bookshelf/EnviromentTest.java
@@ -0,0 +1,20 @@
+package de.workshops.bookshelf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@ActiveProfiles(profiles = {"prod"})
+class EnviromentTest {
+    @Value("${server.port}")
+    private int port;
+
+    @Test
+    void verifyProdPort() {
+        assertEquals(8090, port);
+    }
+}
